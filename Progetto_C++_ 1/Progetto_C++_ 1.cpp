e42d38f669a8f3c10aedd1f6829322bd6e80a707
Simone <bassanelli.simone.studente@itispaleocapa.it>
2023-04-15 09:55:06 +0200

    ver2

--- Progetto_C++_ 1/Progetto_C++_ 1.cpp
+++ Progetto_C++_ 1/Progetto_C++_ 1.cpp
@@ -10,7 +10,7 @@ string RicettaCompleta(string nome)
     string s = "", nomeTemp, ricetta;
 
     ifstream Ricetta("ricettario.csv");
-    getline(Ricetta, s);
+    std::getline(Ricetta, s);
     while (!s.empty()) 
     {
         nomeTemp=s.substr(0, s.find(";")); //dividere la stringa per ottenere il nome della ricetta 
@@ -19,21 +19,86 @@ string RicettaCompleta(string nome)
             ricetta = s;
             break;
         }
-        getline(Ricetta, s);
+        std::getline(Ricetta, s);
     }
     return ricetta;
 }
 
-int ListaSpesa(string ingrediente, float quantità)
-{
-    fstream Lista("listaspesa.csv", ios::app);
 
-    Lista << ingrediente << ":" << quantità;
+
+void ListaSpesa(string nome, float quantita)
+{
+    bool trovato=false;
+    string s, nomeTemp="";
+    int quantitaTemp;
+    ifstream ListaRead("listaspesa.csv", ios::app);
+    ofstream ListaWrite("listaspesaTemp.csv", ios::app);
+
+
+    //ListaWrite << ingrediente << ":" << quantità;
     //cout << ceil(quantità / 500);
 
-    int ricavo = ceil(quantità / 500) * 500 - quantità;
-    Lista.close();
-    return ricavo;
+    //int ricavo = ceil(quantità / 500) * 500 - quantità;
+
+
+
+    std::getline(ListaRead, s);
+
+
+    //controllo se trovo l'ingrediente nel file
+    while (!s.empty())
+    {
+		nomeTemp = s.substr(0, s.find(":"));
+		quantitaTemp = stoi(s.substr(s.find(":") + 1, s.length()));
+        if (nome == nomeTemp)
+        {
+			trovato = true;
+			//scala quantità e lista della spesa
+			quantita = quantita + quantitaTemp;
+			ListaWrite << nome << ":" << quantita << endl;
+		}
+        else
+        {
+            ListaWrite << nomeTemp << ":" << quantitaTemp << endl;
+        }
+
+        
+		//Fai il nuovo file copia e aggiungi
+		std::getline(ListaRead, s);
+	}
+    /* {
+        nomeTemp = s.substr(0, s.find(";"));
+        quantitaTemp = stoi(s.substr(s.find(":") + 1, s.length()));
+
+        if (nome == nomeTemp)
+        {
+            trovato = true;
+            //scala quantità e lista della spesa
+            quantita = quantita + quantitaTemp;
+
+            ListaWrite << nome << ":" << quantita << endl;
+
+        }
+
+        //Fai il nuovo file copia e aggiungi
+        std::getline(ListaRead, s);
+    }*/
+
+    if (!trovato)
+    {
+        //aggiungi ingrediente:0 e lista della spesa
+        ListaWrite << nome << ":" << quantita << endl;
+    }
+
+    ListaRead.close();
+    ListaWrite.close();
+
+    std::remove("listaspesa.csv");
+    std::rename("listaspesaTemp.csv", "listaspesa.csv");
+    std::remove("listaspesaTemp.csv");
+
+    
+    
 }
 
 
@@ -57,24 +122,6 @@ void Magazzino()
 
 
 
-/*string* ControlloIngredienti(string ingredienti)
-{
-    int dim = 1;
-    string* nomi_e_ingre = new string[100];
-
-    while (ingredienti.find(",") != string::npos)
-    {
-        nomi_e_ingre[dim] = ingredienti.substr(0, ingredienti.find(","));
-        dim++;
-        ingredienti = ingredienti.erase(0, ingredienti.find(",") + 1);
-        //cout << "\ningredienti rimanenti: " << ingredienti;
-    }
-    nomi_e_ingre[dim] = ingredienti;
-    dim++;
-    nomi_e_ingre[0] = dim;
-    //prima pos array è per la lunghezza
-    return nomi_e_ingre;
-}*/
 
 
 
@@ -96,7 +143,6 @@ void ControlloMagazzino(string ingredienti)
     int quantita, quantitaTemp;
     bool trovato = false;
     ifstream sr("magazzino.csv");
-    ofstream sw("magazzinoTemp.csv", ios::app);
 
     for (int i = 0; i < dim; i++)
     {
@@ -104,129 +150,16 @@ void ControlloMagazzino(string ingredienti)
         quantita = stoi(nomi_e_ingre[i].substr(nomi_e_ingre[i].find(":") + 1, nomi_e_ingre[i].length()));
 
 
-        getline(sr, s);
-
-
-        //controllo se trovo l'ingrediente nel file
-        while (!nomeTemp.empty())
-        {
-            nomeTemp = s.substr(0, s.find(";"));
-            quantitaTemp = stoi(s.substr(s.find(":") + 1, s.length()));
-
-            if (nome == nomeTemp)
-            {
-                trovato = true;
-                //scala quantità e lista della spesa
-                quantitaTemp = quantitaTemp - quantita;
-
-                if (quantitaTemp < 0)
-                {
-                    quantitaTemp = ListaSpesa(nome, quantitaTemp) + quantitaTemp;
-                }
-
-                sw << nome << ":" << quantitaTemp << endl;
-
-            }
-
-            //Fai il nuovo file copia e aggiungi
-            getline(sr, s);
-        }
-
-
-        if (!trovato)
-        {
-            //aggiungi ingrediente:0 e lista della spesa
-            sw << nome << ":" << ListaSpesa(nome, quantita) << endl;
-        }
-
+        ListaSpesa(nome, quantita); //non somma
     }
 
     sr.close();
-    sw.close();
 
-    remove("magazzino.csv");
-
-    if (rename("magazzinoTemp.csv", "magazzino.csv") != 0)
-        perror("Error renaming file");
-    else
-        cout << "File renamed successfully";
+    
 }
 
 
-/*
-void ControlloMagazzinoNO(string ingredienti)
-{
-    string* nandi= new string[100];
 
-    string nomi_e_ingre[100] = c_str(sandi);
-
-    nomi_e_ingre = ControlloIngredienti(ingredienti);
-
-    int dim = stoi(nomi_e_ingre[0]);
-
-    //abbiamo gli ingredienti salvati su um array
-
-    string nome, nomeTemp, s;
-    int quantita, quantitaTemp;
-    bool trovato = false;
-    ifstream sr("magazzino.csv");
-    ofstream sw("magazzinoTemp.csv", ios::app);
-
-    //salto pos 0 perche è dove è memorizzatála dimensione
-    for (int i = 1; i < dim; i++)
-    {
-        nome = nomi_e_ingre[i].substr(0, nomi_e_ingre[i].find(":"));
-        quantita = stoi(nomi_e_ingre[i].substr(nomi_e_ingre[i].find(":") + 1, nomi_e_ingre[i].length()));
-
-
-        getline(sr, s);
-
-
-        //controllo se trovo l'ingrediente nel file
-        while (!nomeTemp.empty())
-        {
-            nomeTemp = s.substr(0, s.find(";"));
-            quantitaTemp = stoi(s.substr(s.find(":") + 1, s.length()));
-
-            if (nome == nomeTemp)
-            {
-                trovato = true;
-                //scala quantità e lista della spesa
-                quantitaTemp = quantitaTemp - quantita;
-
-                if (quantitaTemp < 0)
-                {
-                    quantitaTemp = ListaSpesa(nome, quantitaTemp) + quantitaTemp;
-                }
-
-                sw << nome << ":" << quantitaTemp << endl;
-
-            }
-
-            //Fai il nuovo file copia e aggiungi
-            getline(sr, s);
-        }
-
-
-        if (!trovato)
-        {
-            //aggiungi ingrediente:0 e lista della spesa
-            sw << nome << ":" << ListaSpesa(nome, quantita) << endl;
-        }
-
-    }
-
-    sr.close();
-    sw.close();
-
-    remove("magazzino.csv");
-
-    if (rename("magazzinoTemp.csv", "magazzino.csv") != 0)
-        perror("Error renaming file");
-    else
-        cout << "File renamed successfully";
-    delete[](nomi_e_ingre);
-}*/
 
 
 int main()
@@ -235,32 +168,37 @@ int main()
     MyFile << "test";
     MyFile.close();*/
     string ingredienti;
-    int scelta;
+    int scelta=-1;
     Magazzino();
     cout << "Buongiorno, che dolce desidera ordinare?\n";
-    cout << "1 - Sacher\n 2 - Creme Brulee";
-    cin >> scelta;
+    cout << "1 - Sacher\n 2 - Creme Brulee\n 3 - Crostata\n";
     
-    switch (scelta)
+    while (scelta != 0) 
     {
-    case 1:
-        ingredienti = Ingredienti(RicettaCompleta("sacher"));
-        ControlloMagazzino(ingredienti);
-        break;
-    case 2:
+        cin >> scelta;
+        switch (scelta)
+        {
+        case 1:
+            ingredienti = Ingredienti(RicettaCompleta("sacher"));
+            ControlloMagazzino(ingredienti);
+            break;
+        case 2:
+            ingredienti = Ingredienti(RicettaCompleta("creme brulee"));
+            ControlloMagazzino(ingredienti);
+            break;
+        case 3:
+            ingredienti = Ingredienti(RicettaCompleta("crostata"));
+            ControlloMagazzino(ingredienti);
+            break;
+        case 4:
 
-        break;
-    case 3:
-
-        break;
-    case 4:
-
-        break;
+            break;
+        }
     }
-
-
     
-    std::remove("listaspesa.csv");
+    //rinominare il file vecchio
+    std::remove("listaspesaVecchia.csv");
+    std::rename("listaspesa.csv", "listaspesaVecchia.csv");
     std::remove("magazzinoTemp.csv");
 }
 
